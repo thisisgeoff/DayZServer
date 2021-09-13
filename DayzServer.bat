4fdba456f45b5cd46a7edd65a491f33c03fd1579
Geoff <geoffrey.adams@gmail.com>
2021-09-13 06:58:15 -0300

    Heroes and Bandits

--- DayzServer.bat
+++ DayzServer.bat
@@ -13,7 +13,7 @@ set serverProfile=config
 ::Logical CPU cores to use (Equal or less than available)
 set serverCPU=4
 ::mods (@modName;@anotherModname;@mod)
-set mods=@CF;@eAI;@Namalsk Island;@Namalsk Survival;@Community-Online-Tools;@Dabs Framework;@SkyZ - Skybox Overhaul;@sVisual;@sGunplay;@DayZ-Expansion-Bundle;@DayZ-Expansion-Licensed;@Forced First Person Combat;@StaminaSettings;@AdditionalFrozenFoodNamalsk;@Increased Lumens;@BetterClutterCutters;@BadgerBuildings;@BuildingsMegaModPack;@BuildingsModPack5;@BuildingsModPack6;@IndustrialModPack;@Mists (Atmosphere);@Forever_Lights_and_Campfires;@ParticlePointsPlacement;@ZStuff;@DayZ Editor Loader;@Buggy;@dbo_surfaces_nam;@Blackouts Corvette;@GoreZ;@GasMaskAddons;@RadZone;@WindstridesClothingPack;@AlienMoon;@FS;@FS_English_Translation;@OV_AllInOne;@AmbientLight;@Nehr_Stuff;
+set mods=@CF;@eAI;@Namalsk Island;@Namalsk Survival;@Community-Online-Tools;@Dabs Framework;@SkyZ - Skybox Overhaul;@sVisual;@sGunplay;@DayZ-Expansion-Bundle;@DayZ-Expansion-Licensed;@Forced First Person Combat;@StaminaSettings;@AdditionalFrozenFoodNamalsk;@Increased Lumens;@BetterClutterCutters;@BadgerBuildings;@BuildingsMegaModPack;@BuildingsModPack5;@BuildingsModPack6;@IndustrialModPack;@Mists (Atmosphere);@Forever_Lights_and_Campfires;@ParticlePointsPlacement;@ZStuff;@DayZ Editor Loader;@Buggy;@dbo_surfaces_nam;@Blackouts Corvette;@GoreZ;@GasMaskAddons;@RadZone;@WindstridesClothingPack;@AlienMoon;@FS;@FS_English_Translation;@OV_AllInOne;@Nehr_Stuff;@Heroes And Bandits;
 ::mods (@modName;@anotherModname;@mod)
 ::set servermods=
 ::Sets title for terminal (DONT edit)
